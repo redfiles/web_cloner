d74108089f0ea48c7dbc2c3c6a7e2be45f54d9bd
hackerz <89154577+redfiles@users.noreply.github.com>
2022-08-02 05:37:22 -0400

    Add files via upload

---
+++ web_cloner.py
@@ -0,0 +1,32 @@
+import requests
+from argparse import ArgumentParser
+
+parser = ArgumentParser();
+
+parser.add_argument('-u', '--url', type=str, help='Enter the URL (Eg:- https://google.com/ , https://example.com/index.php )');
+parser.add_argument('-f', '--filename', type=str, help='Give any name to make file. (Eg:- example.html , example.php)')
+
+args = parser.parse_args();
+
+def clone():
+	url = args.url;
+	filen = args.filename;
+	print("Web Cloner v1.0.0")
+	print("---------------------------------------------------------------");
+	print("---------------------------------------------------------------");
+	print("Running...");
+	print("---------------------------------------------------------------");
+	print(" ");
+	response=requests.get(url);
+	print("---------------------------------------------------------------");
+	print("");
+	x = open("./webpages/"+filen,"w");
+	x.write(str(response.text));
+	x.close();
+	print("File created.");
+	print("Path : /webpages/"+str(filen));
+
+
+clone();
+print("---------------------------------------------------------------");
+print("Bye Bye ...");
